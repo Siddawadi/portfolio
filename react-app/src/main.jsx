8f2f7277fafa82f467c25ee860cf22a88af4a792
siddharthadawadi <siddharthadawadi10@gmail.com>
2026-05-26 19:00:37 +0545

    render

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
-import './app.css'
+import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    port:3000,
-  }
-})
+  plugins: [react(), tailwindcss()],
+
+  base: "/portfolio/",   // 🔥 IMPORTANT (repo name only)
+
+  server: {
+    port: 3000,
+  },
+});
