5f0cdb559a8ea42992d375de13654271885e7590
siddharthadawadi <siddharthadawadi10@gmail.com>
2026-05-26 19:03:48 +0545

    render

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: "/portfolio/",   // 🔥 IMPORTANT (repo name only)
+  base: "/",   
 
   server: {
     port: 3000,
